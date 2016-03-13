2892ad65361b59273c6d46b78a6eb43280ac9cd9
Jay Doane <jay.s.doane@gmail.com>
2016-03-13 14:33:48 -0700

    Supervise nitrogen_sup and tcp_server_app with subdomain_sup

--- src/subdomain_sup.erl
+++ src/subdomain_sup.erl
@@ -28,7 +28,24 @@ start_link() ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
+    {ok, { {one_for_one, 0, 1}, [
+        {
+            nitrogen_sup,
+            {nitrogen_sup, start_link, []},
+            permanent,
+            infinity,
+            supervisor,
+            [nitrogen_sup]
+        },
+        {
+            tcp_server_app,
+            {tcp_server_app, start, [ignore, ignore]},
+            permanent,
+            infinity,
+            supervisor,
+            [tcp_server_app]
+        }
+    ]}}.
 
 %%====================================================================
 %% Internal functions
